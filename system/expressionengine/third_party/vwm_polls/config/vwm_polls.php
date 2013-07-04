4f44326aea2ff31c3629f051ee93ffc70944ac09
Victor Michnowicz <vmichnowicz@yahoo.com>
2013-07-03 21:58:08 -0400

    Change defauld config, add comment

--- system/expressionengine/third_party/vwm_polls/config/vwm_polls.php
+++ system/expressionengine/third_party/vwm_polls/config/vwm_polls.php
@@ -5,7 +5,7 @@
  * already voted. This can cause some issues when multiple users share the same IP address.
  */
 $config['vwm_polls_check_ip_address'] = FALSE; //TRUE;
-$config['vwm_polls_check_javascript_attributes'] = TRUE; // Check "unique" JavaScript attributes
-$config['vwm_polls_template_prefix'] = '';
+$config['vwm_polls_check_javascript_attributes'] = FALSE; // Check "unique" JavaScript attributes
+$config['vwm_polls_template_prefix'] = ''; // Something like "vwm_polls_"
 
 // EOF
